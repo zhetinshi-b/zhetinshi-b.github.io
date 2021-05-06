610f97542b88332176802b30e5441a39e43e0ae5
zhetinshi-b <83744202+zhetinshi-b@users.noreply.github.com>
2021-05-06 10:18:05 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var ops = document.getElementById("ops");
+
+ops.onclick = function () {
+    alert("Вы уже на русской версии сайта!")
+}
+var ops1 = document.getElementById("ops1");
+
+ops1.onclick = function () {
+    alert("Вы уж простите, но мне лень делать еще и Казахскую версию сайта!")
+}
